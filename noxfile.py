a96655ca66502e223ea4130dc4a90ffa0ece4500
12rambau <pierrick.rambaud49@gmail.com>
2023-01-10 22:52:19 +0100

    use nox add nox to run test and mypy in isolated environment

---
+++ noxfile.py
@@ -0,0 +1,24 @@
+"""Test process to run in isolated environments."""
+
+import nox
+
+
+@nox.session(python=["3.7", "3.8", "3.9"], reuse_venv=True)
+def test(session):
+    """Apply the tests on the lib."""
+    session.install(".[test]")
+    session.run("pytest", "--color=yes", "tests")
+
+@nox.session(name="mypy", reuse_venv=True)
+def mypy(session):
+    """Run the mypy evaluation of the lib."""
+    session.install(".")
+    session.install("mypy")
+    test_files = session.posargs or ["sphinx-favicon"]
+    session.run(
+        "mypy",
+        "--ignore-missing-imports",
+        "--install-types",
+        "--non-interactive",
+        *test_files,
+    )
