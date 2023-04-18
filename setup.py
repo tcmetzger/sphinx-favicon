8f3057451027bfe637429dccad40c60dc2460f34
Gianfranco Costamagna <costamagnagianfranco@yahoo.it>
2023-04-18 10:15:47 +0200

    Update setup.py
    Actually install the python scripts

--- setup.py
+++ setup.py
@@ -1,3 +1,18 @@
-import setuptools
+from setuptools import setup
 
-setuptools.setup()
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+   name='sphinx_favicon',
+   version='1.0.1',
+   description='Add custom favicons to your Sphinx html documentation quickly and easily.',
+   license="MIT",
+   long_description=long_description,
+   author='Timo Cornelius Metzger',
+   author_email='coding@tcmetzger.net',
+   url="https://github.com/tcmetzger/sphinx-favicon",
+   packages=['sphinx_favicon'],
+   install_requires=[],
+   scripts=[]
+)
